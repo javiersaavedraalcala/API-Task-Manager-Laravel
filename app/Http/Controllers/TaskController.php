a7a2199b9d606b86c0fa5efb2c48014f08b5d57d
Javier Saavedra <javiersaavedra@Javiers-Mac-mini.local>
2024-04-01 17:56:06 -0600

    First commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = QueryBuilder::for(Task::class)
+            ->allowedFilters(['title', 'is_done'])
+            ->defaultSort('created_at')
+            ->allowedSorts(['title', 'is_done', 'created_at'])
+            ->get();
+
+        return new TaskCollection($tasks);
+    }
+
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Auth::user()->tasks()->create($request->validated());
+
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return new TaskResource($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'is_done',
+        'project_id'
+    ];
+
+    protected $cast = [
+        'is_done' => 'boolean'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
+
+    public function crator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('creator', function(Builder $builder) {
+            $builder->where('creator_id', Auth::id());
+        });
+    }
+}
